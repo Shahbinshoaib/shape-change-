88ceb9d0b98f1de5181b9e8805e47b7cf1e61322
ARehman212 <abdulrehmanarm3975@gmail.com>
2024-09-14 20:47:33 +0500

    upload

--- Shape Change/Assets/Script/MovePath.cs
+++ Shape Change/Assets/Script/MovePath.cs
@@ -5,6 +5,8 @@ using UnityEngine;
 public class MovePath : MonoBehaviour
 {
     public float speed;
+
+    private float boundry = 170;
     // Start is called before the first frame update
     void Start()
     {
@@ -14,6 +16,10 @@ public class MovePath : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        transform.Translate(Vector3.left * speed * Time.deltaTime);
+        transform.Translate(Vector3.right * speed * Time.deltaTime);
+        if (transform.position.x > boundry)
+        {
+            Destroy(gameObject);
+        }
     }
 }
