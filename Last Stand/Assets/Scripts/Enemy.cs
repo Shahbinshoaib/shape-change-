c1cf5987c118e153e2de81155f65f79b5e79fee5
ARehman212 <abdulrehmanarm3975@gmail.com>
2024-09-14 17:46:43 +0500

    upload

---
+++ Last Stand/Assets/Scripts/Enemy.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    public float speed = 3.0f;
+    private GameObject Player;
+    private Rigidbody enemyRb;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        enemyRb = GetComponent<Rigidbody>();
+        Player = GameObject.Find("Player");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 lookDirection = (Player.transform.position - transform.position).normalized;
+        enemyRb.AddForce(lookDirection * speed);
+    }
+}
